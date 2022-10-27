101e799d9a038d170a4c5fcf412bc4629a3bbb94
roxannaaguilera <roxaguileo@gmail.com>
2022-10-27 10:50:44 -0300

    homework.IV.3

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -105,6 +105,9 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+  usuario.amigos.push('Shay');
+  return usuario;
+
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -113,6 +116,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0; i < usuarios.length; i++) {
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -122,6 +129,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0;
+  for (var i = 0; i < usuario.posts.length; i++) {
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -134,7 +146,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function () {
+    return this.precio - (this.precio * this.porcentajeDeDescuento);
+    return calcularPrecioDescuento;
+  };
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
